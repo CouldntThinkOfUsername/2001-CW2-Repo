f59164ef684280722152d84a78e119b9678c63bb
DemonicCheese <JamieWoodacre@outlook.com>
2025-12-22 14:04:47 +0000

    Added all CRUD to users.py and updated yml

--- Authentication_API/users.py
+++ Authentication_API/users.py
@@ -12,3 +12,39 @@ def read_one(user_id):
         return user_schema.dump(user)
     else:
         abort(404, f"User {user_id} not found")
+
+def create(User):
+    user_id = User.get("user_id")
+    existing_User = User.query.filter(User.user_id == user_id).one_or_none()
+
+    if existing_User is None:
+        new_User = users_schema.load(User, session=db.session)
+        db.session.add(new_User)
+        db.session.commit()
+        return users_schema.dump(new_User), 201
+    else:
+        abort(406, f"User with id: {user_id} already exists")
+
+
+def update(user_id, User):
+    existing_User = User.query.filter(User.user_id == user_id).one_or_none()
+
+    if existing_User:
+        update_User = users_schema.load(User, session=db.session)
+        existing_User.fname = update_User.fname
+        db.session.merge(existing_User)
+        db.session.commit()
+        return users_schema.dump(existing_User), 201
+    else:
+        abort(404, f"User with id: {user_id} not found")
+
+
+def delete(user_id):
+    existing_User = User.query.filter(User.user_id == user_id).one_or_none()
+
+    if existing_User:
+        db.session.delete(existing_User)
+        db.session.commit()
+        return make_response(f"{user_id} successfully deleted", 200)
+    else:
+        abort(404, f"User with id: {user_id} not found")
